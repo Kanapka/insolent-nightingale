db705add4a884284aa0848ebdc91e35a3e80a7c5
evilturnip <trulyevilturnip@gmail.com>
2019-08-13 19:16:29 +0200

    define commands for movement

---
+++ Desktop/Desktop/Desktop/Commands/MobilityCommand.cs
@@ -0,0 +1,20 @@
+﻿using Desktop.Modules;
+
+namespace Desktop.Commands
+{
+    class MobilityCommand : ICommand
+    {
+        public string Payload { get; }
+        public IModule Module => new MobilityModule();
+        private MobilityCommand(string _payload)
+        {
+            Payload = _payload;
+        }
+
+        public MobilityCommand Forward => new MobilityCommand("Forward");
+        public MobilityCommand Reverse => new MobilityCommand("Reverse");
+        public MobilityCommand Left => new MobilityCommand("Left");
+        public MobilityCommand Right => new MobilityCommand("Right");
+        public MobilityCommand Stop => new MobilityCommand("Stop");
+    }
+}
