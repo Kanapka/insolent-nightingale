07fa9e6d9681b1bbca9156432574f4d2b1bc8939
evilturnip <trulyevilturnip@gmail.com>
2019-09-02 21:30:35 +0200

    mobility module keeps track of current movement status

--- Server/Server/Physical/MobilityModule.py
+++ Server/Server/Physical/MobilityModule.py
@@ -4,10 +4,17 @@ from Communication.Message import Message, MessageType
 from Communication.EventBus import EventBus
 
 class MobilityModule(BaseModule):
+    
+    forward = "Forward"
+    backward = "Backward"
+    left = "Left"
+    right = "Right"
+    stopped = "Stopped"
 
     def __init__(self, robot: Robot, event_bus: EventBus):
         self.robot = robot
         self.event_bus = event_bus
+        self.current = MobilityModule.stopped
 
     def power_up(self):
         super().power_up()
@@ -23,34 +30,41 @@ class MobilityModule(BaseModule):
     
     def process(self, message: Message):
         print(f'Mobility module recieved a message {message.payload}')
-        if message.payload == 'Forward':
-            self.forward()
-        elif message.payload == 'Backward':
-            self.backward()
-        elif message.payload == 'Left':
-            self.left()
-        elif message.payload == 'Right':
-            self.right()
+        if(message.payload == self.current):
+            pass
         else:
-            self.stop_moving()
+            if message.payload == MobilityModule.forward:
+                self.forward()
+            elif message.payload == MobilityModule.backward:
+                self.backward()
+            elif message.payload == MobilityModule.left:
+                self.left()
+            elif message.payload == MobilityModule.right:
+                self.right()
+            else:
+                self.stop_moving()
 
     def forward(self):
+        self.current = MobilityModule.forward
         self.robot.forward()
         print ("Going forward")
 
     def backward(self):
+        self.current = MobilityModule.backward
         print ("Reversing")
         self.robot.backward()
 
     def right(self):
+        self.current = MobilityModule.right
         print ("Turning right")
         self.robot.right()
 
     def left(self):
+        self.current = MobilityModule.left
         print ("Turning left")
         self.robot.left()
 
     def stop_moving(self): 
+        self.current = MobilityModule.stopped
         print ("Stopped moving")
         self.robot.stop()
-
